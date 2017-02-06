ae1cd5ac8d6bf4f480982e1a16b4f9e3aa720ea9
naieemsupto <naieem.mahmud@selise.ch>
2017-02-06 16:39:34 +0600

    complete new react application

---
+++ App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class App extends React.Component {
+	constructor() {
+		super();
+		
+		this.state = {
+			header: "Header from state...",
+			"content": "Content from state..."
+		}
+		this.data = {
+			name : "name",
+			title : "title",
+			// Hello : hello
+		}
+		this.change = this.update.bind(this);
+  }
+
+  update(ev) {
+  	console.log('test');
+    // this.setState({ text: ev.target.value });
+  }
+	render() {
+		return (
+			<div>
+			<h1>{this.state.header}</h1>
+			<h2>{this.state.content}</h2>
+			<p onClick={this.change}>{this.data.name}</p>
+			<p>{this.data.title}</p>
+			</div>
+			);
+	}
+}
+
+export default App;
